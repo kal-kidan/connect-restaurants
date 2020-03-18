ade0878c27436082f4ebf64d1aa3dacfd820d7ac
kalkidan <kal05627356@gmail.com>
2020-03-18 10:42:24 +0300

    setPasswordAttribute added to bcrypt the password

--- laravelpart/app/User.php
+++ laravelpart/app/User.php
@@ -51,4 +51,7 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+    public function setPasswordAttribute($value){
+         $this->attributes['password']=bcrypt($value);
+    }
 }
