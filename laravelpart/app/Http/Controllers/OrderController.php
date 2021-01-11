5b387f56aa15b1fe60cc2f4c3427749c1dd9ef14
kal-kidan <kal05627356@gmail.com>
2021-01-11 15:36:37 -0500

    cart cleared on order

--- laravelpart/app/Http/Controllers/OrderController.php
+++ laravelpart/app/Http/Controllers/OrderController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
 use App\Order;
+use App\Cart;
 use App\OrderItem;
 class OrderController extends Controller
 {
@@ -40,12 +41,12 @@ class OrderController extends Controller
         }
        }
         try { 
+         $deletedRow = Cart::where('user_id', '=', $request->order['user_id'])->delete(); 
          $order = Order::create($request->order);  
         if($order){
             $orderItems = [];
             for ($i=0; $i < count($request->orderItems); $i++) { 
                 OrderItem::create(["order_id"=>$order['id'], "menu_id"=>$request->orderItems[$i]['menu_id'], "price"=>$request->orderItems[$i]['price'], "name"=>$request->orderItems[$i]['name'], "quantity"=>$request->orderItems[$i]['quantity']]);
-                // array_push($orderItems, new OrderItems($request->orderItems[$i]['menu_id'], $request->orderItems[$i]['user_id'], $request->orderItems[$i]['price'], $request->orderItems[$i]['name'], $request->orderItems[$i]['quantity'], $order['id']));
             }   
             return response()->json(["status"=>true, "message"=>"order placed successfully", "order"=>$order]);
         }
@@ -57,8 +58,7 @@ class OrderController extends Controller
         }
     }
 
-    public function getVendorOrders($vendor_id){
-       // ->join('order_items', 'orders.id', '=', 'order_items.order_id')
+    public function getVendorOrders($vendor_id){ 
         $orders = Order::where('vendor_id', $vendor_id)
         ->join('users', 'users.id', '=', 'orders.user_id')
         ->select('orders.*', 'users.firstName', 'users.lastName', 'users.address', 'users.phoneNumber', 'users.email')
