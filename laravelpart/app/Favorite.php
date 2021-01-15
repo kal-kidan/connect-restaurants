3147281bd873d77a4fb14df4131ba16ad1483d60
kal-kidan <kal05627356@gmail.com>
2021-01-15 14:26:31 -0500

    favorite model, controller, migration created

---
+++ laravelpart/app/Favorite.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Favorite extends Model
+{
+    protected $fillable = ['user_id', 'vendor_id', 'created_at', 'updated_at'];
+}
